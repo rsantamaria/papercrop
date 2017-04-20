7415d8ad521960a7781911cb93842edbbbb40c87
oiorain <marion.bouguet@gmail.com>
2017-04-20 11:40:43 +0900

    Update for modern version of Capybara
    Capybara needs to be told to look for an hidden element now.

--- spec/integration/papercrop_spec.rb
+++ spec/integration/papercrop_spec.rb
@@ -15,21 +15,21 @@ describe "Image crop" do
     page.should have_css("#picture_crop_preview")
     page.should have_css("#picture_cropbox")
 
-    page.should have_css("#landscape_picture_original_w")
+    page.should have_css("#landscape_picture_original_w", visible: false)
 
-    find("#landscape_picture_original_w").value.should eq("1024")
-    find("#landscape_picture_original_h").value.should eq("768")
-    
+    find("#landscape_picture_original_w", visible: false).value.should eq("1024")
+    find("#landscape_picture_original_h", visible: false).value.should eq("768")
+
     find("#picture_cropbox")[:"data-box-width"].should eq("600")
     find("#picture_cropbox")[:"data-aspect-ratio"].should eq((4.0 / 3.0).to_s)
 
-    find("#picture_crop_x").set "300"
-    find("#picture_crop_y").set "200"
-    find("#picture_crop_w").set "400"
-    find("#picture_crop_h").set "300"
+    find("#picture_crop_x", visible: false).set "300"
+    find("#picture_crop_y", visible: false).set "200"
+    find("#picture_crop_w", visible: false).set "400"
+    find("#picture_crop_h", visible: false).set "300"
 
     click_button "Crop image"
-    
+
     compare_images(expected_mountains_img_path, Landscape.last.picture.path(:medium)).round(2).should eq(0.0)
   end
 end
